bd22538fbb1a7f78ed8ba56fb33773f673761e59
sanjay-kv2769 <93617720+sanjay-kv2769@users.noreply.github.com>
2024-07-24 10:49:28 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
       <Features />
       {/* <Clients /> */}
       <Contact />
-      <Review />
+      {/* <Review /> */}
       <Testimonials />
       <Articles />
       <button
